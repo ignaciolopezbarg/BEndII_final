b277472ea98c7a45d2167e40047c552a494bf4c3
ignacio lopez barg <ignaciolopezbar@hotmail.com>
2024-09-14 17:42:12 -0300

    dao

--- src/dao/mongoDBFunkoDao.js
+++ src/dao/mongoDBFunkoDao.js
@@ -19,34 +19,3 @@ try {
     }
 }
 export default MongoDBFunkoDAO;
-/////////////////////////////////////////////////
-// class MongoDBFunkoDAO {
-//     async crearFunko(funkoDTO) {
-//         try {
-//             console.log("DTO recibido en MongoDBFunkoDAO:", funkoDTO);
-            
-//             const funkoData = {
-//                 nombre: funkoDTO.nombre,
-//                 categoria: funkoDTO.categoria,
-//                 fullname: funkoDTO.fullname,
-//                 precio: funkoDTO.precio,
-//                 codigo: funkoDTO.codigo,
-//                 descripcion: funkoDTO.descripcion,
-//                 stock: funkoDTO.stock
-//             };
-            
-//             console.log("Datos a guardar en MongoDB:", funkoData);
-            
-//             const funko = new ProductoModel(funkoData);
-//             const savedFunko = await funko.save();
-            
-//             console.log("Funko guardado en MongoDB:", savedFunko);
-            
-//             return savedFunko;
-//         } catch (error) {
-//             console.error("Error al crear el funko en MongoDB:", error);
-//             throw new Error("Error al crear el funko en MongoDB: " + error.message);
-//         }
-//     }
-// }
-// export default MongoDBFunkoDAO;
